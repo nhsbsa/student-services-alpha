a3ce600ea7f3812b493f0e1c37ace33faf87f3a2
Joe Hutchinson NHSBSA <55741037+joe-hutchinson@users.noreply.github.com>
2025-07-16 17:05:02 +0100

    CCS SB payment journey

--- app/views/BSA-internal/v1/_routes.js
+++ app/views/BSA-internal/v1/_routes.js
@@ -16,17 +16,7 @@ const router = express.Router();
 // Student search //
 
 router.post('/v1/student-search', (req, res) => {
-    const funding = req.session.data['funding'];
-
-    if (!funding || !Array.isArray(funding) || funding.length === 0) {
-        // Handle empty or invalid funding
-        req.session.data['funding'] = '';
-    } else if (funding.includes('Learning Support Fund')) {
-        req.session.data['funding'] = 'Learning Support Fund';
-    } else if (funding.includes('Student Bursary')) {
-        req.session.data['funding'] = 'Student Bursary';
-    }
-
+    req.session.data['search'] = 'true';
     res.redirect('/BSA-internal/v1/student-search');
 });
 
